041feb47e40ed6f60d91ef63aaa21775721a7fb9
Olga Lukashenko <lukashenko.93@bk.ru>
2022-08-07 12:15:43 +0800

    Initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTest.java
@@ -0,0 +1,32 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testName() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragment")
+                )
+        );
+    }
+}
